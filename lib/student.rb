eda5a08034527bf7332b723ae8a2abb7d9ca7bc0
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-07-19 17:40:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    student_array.each do |student|
+    students_array.each do |student|
       self.new(student)
     end
   end
